b524f73f0ec6293d5fb5d741c4815a3b0b5ab867
nb0359 <nb0359@pleasantonusd.net>
2025-09-03 10:40:09 -0700

    Update Driver.java
    Latest changes

--- Driver.java
+++ Driver.java
@@ -1,55 +1,59 @@
-import java.util.Scanner;  //may be necessary for input
+import java.util.Scanner; //may be necessary for input
 import java.io.File;
 import java.io.FileNotFoundException;
 
-import javax.swing.JOptionPane;  //may be necessary for input
+import javax.swing.JOptionPane; //may be necessary for input
 
 import kareltherobot.*;
 
 public class Driver implements Directions {
-// declared here so it is visible in all the methods!! 
-// It will be assigned a value in the getInfo method
-	private static Robot roomba; 
-	private static int width, height, area;
+	// declared here so it is visible in all the methods!!
+	// It will be assigned a value in the getInfo method
+	public static Robot roomba;
+	public static int picked = 0;
+	public static int numPiles = 0;
+	public static int largest_pile_size = 0;
+	public static int largest_pile_size_street, largest_pile_size_ave;
 
+	private static int width, height, area;
 
 	// You will add very many variables!!
 
-	
 	public static void main(String[] args) throws FileNotFoundException {
-			// LEAVE THIS ALONE!!!!!!
-			Driver d = new Driver();
+		// LEAVE THIS ALONE!!!!!!
+		Driver d = new Driver();
 
-  /**
-	 * This section of code gets info from the user in the following order: 1. Ask the user
-	 * which world file they wish to process. Right now, that world file name is
-	 * hardcoded in. 2. Ask the user for the starting location and direction of the
-	 * Robot. A new Robot should be constructed and assigned to the global
-	 * (instance) variable named roomba that is declared on line 10.
-	 * 
-	 * An inelegant way to interact with the user is via the console, using
-	 * System.out.println and a Scanner that scans System.in (input from the
-	 * console). A more elegant way to get user input might include using a
-	 * JOptionPane.
-	 */
-    	
+		/**
+		 * This section of code gets info from the user in the following order: 1. Ask
+		 * the user
+		 * which world file they wish to process. Right now, that world file name is
+		 * hardcoded in. 2. Ask the user for the starting location and direction of the
+		 * Robot. A new Robot should be constructed and assigned to the global
+		 * (instance) variable named roomba that is declared on line 10.
+		 * 
+		 * An inelegant way to interact with the user is via the console, using
+		 * System.out.println and a Scanner that scans System.in (input from the
+		 * console). A more elegant way to get user input might include using a
+		 * JOptionPane.
+		 */
 
-	/** This section will have all the logic that takes the Robot to every location 
-	 * and cleans up all piles of beepers.  Think about ways you can break this
-	 * large, complex task into smaller, easier to solve problems.
-	 */
+		/**
+		 * This section will have all the logic that takes the Robot to every location
+		 * and cleans up all piles of beepers. Think about ways you can break this
+		 * large, complex task into smaller, easier to solve problems.
+		 */
 
 		// the line below causes a null pointer exception
 		// what is that and why are we getting it?basicRoom.
 
+		/**
+		 * This method displays the results of cleaning the room. All of the info
+		 * in the pdf that describes the problem need to be displayed. You can present
+		 * this info in the console (boring) or you can present using JOptionPane
+		 * (cool!)
+		 */
 
-  	/** This method displays the results of cleaning the room.  All of the info
-	 * in the pdf that describes the problem need to be displayed.  You can present
-	 * this info in the console (boring) or you can present using JOptionPane (cool!)
-	 */
-
-
-		//Reading the World file name
+		// Reading the World file name
 		System.out.println("The biggest pile was ");
 		Scanner scworldScanner = new Scanner(System.in);
 		System.out.println("Enter the world file: ");
@@ -65,7 +69,7 @@ public class Driver implements Directions {
 			String line = sc_file.nextLine();
 			System.out.println(line);
 		}
-		
+
 		World.readWorld(worldName);
 		World.setVisible(true);
 
@@ -82,34 +86,98 @@ public class Driver implements Directions {
 		System.out.println("What is the start direction? ");
 		Scanner dirworldScanner = new Scanner(System.in);
 		String start_direction = dirworldScanner.nextLine();
-	    System.out.println("The start direction is: " + start_direction);
+		System.out.println("The start direction is: " + start_direction);
+		Direction dir = East;
+		if (start_direction.startsWith("N"))
+			dir = North;
+		else if (start_direction.startsWith("E"))
+			dir = East;
+		else if (start_direction.startsWith("S"))
+			dir = South;
+		else if (start_direction.startsWith("W"))
+			dir = West;
 
-		Robot roomba = new Robot(start_street, start_avenue, North, 0);
-		roomba.move();
+		roomba = new Robot(start_street, start_avenue, dir, 0);
 
-		System.out.println("Robot ready at (" + start_street + ", " + start_avenue + ") facing " + start_direction + ".");
-        System.out.println("World File Name is: " + worldName);
+		System.out
+				.println("Robot ready at (" + start_street + ", " + start_avenue + ") facing " + start_direction + ".");
+		System.out.println("World File Name is: " + worldName);
 
-        // Close scanners (optional; JVM exit will close stdin)
-        strworldScanner.close();
-        aveworldScanner.close();
-        dirworldScanner.close();
+		// Close scanners
+		strworldScanner.close();
+		aveworldScanner.close();
+		dirworldScanner.close();
 
-		width = World.numberOfAvenues();     // avenues (x)
-		height = World.numberOfStreets();   // streets (y)
+		width = World.numberOfAvenues();
+		height = World.numberOfStreets();
 		area = width * height;
 		System.out.println("World has a width of" + width + " and a height of " + height + " and in total " + area + ".");
 
-		// First turn east
-		while (roomba.direction() != East) roomba.turnLeft();
+		changeDir(West);
 
-        while (roomba.frontIsClear()) {
+		while (roomba.frontIsClear()) {
+			cleanCell();
 			roomba.move();
-			//clean the cell
 		}
 
-		return;
+		changeDir(South);
+		roomba.move();
+		changeDir(East);
+		while (roomba.frontIsClear()){
+			cleanCell();
+			roomba.move();
+		}
+			//roomba.move();
+		changeDir(South);
+		roomba.move();
+		changeDir(West);
+		while (roomba.frontIsClear()){
+			cleanCell();
+			roomba.move();
+		}
+			//roomba.move();
+		changeDir(South);
+		roomba.move();
+		changeDir(East);
+		while (roomba.frontIsClear()){
+			cleanCell();
+			roomba.move();
+		}
+		changeDir(South);
+		roomba.move();
+		changeDir(West);
+		while (roomba.frontIsClear()){
+			cleanCell();
+			roomba.move();
+		}
+		
 
+	}
 
-  }
+	public static void changeDir(Direction dir) {
+		while (roomba.direction() != dir) {
+			roomba.turnLeft();
+		}
+	}
+
+	public static void cleanCell() {
+		int current_pile_size = 0;
+		
+		while (roomba.nextToABeeper()) {
+			picked++;
+			current_pile_size++;
+			roomba.pickBeeper();	
+		}
+		if (current_pile_size >= 1 ) {
+			numPiles++;
+		}
+		if (current_pile_size > largest_pile_size) {
+			largest_pile_size = current_pile_size;
+			largest_pile_size_street = roomba.street();
+			largest_pile_size_ave = roomba.avenue();
+		}
+		
+		System.out.println("Picked " + picked + " Number of piles " +  numPiles + " Largest Pile Size " + largest_pile_size);
+		System.out.println("Largest pile ave " + largest_pile_size_ave + " Largest pile street" + largest_pile_size_street);
+	}
 }
